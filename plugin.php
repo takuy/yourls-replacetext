03588b23be582b7b71fe601502aa197aa9ed5a92
Sam <1631095+takuy@users.noreply.github.com>
2024-03-04 12:43:03 -0500

    version up

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: replacetext
 Plugin URI: http://yourls.org/
 Description: replace URL or tokens based on regex pattern matching or script
-Version: 1.0
+Version: 1.1
 Author: takuy
 Author URI: https://github.com/takuy
 */
